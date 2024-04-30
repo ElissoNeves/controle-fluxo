0bfe82cc5e93f4697a814753fb5fee683f436e0a
Elisson Neves <elisson.gnl@gmail.com>
2024-04-30 13:00:47 -0300

    controle de fluxo

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        
+        Scanner scanner =  new Scanner(System.in).useLocale(Locale.US);
+        
+        double saldo = 100.00;
+
+        System.out.println("Digite o valor do saque: ");
+        double valorSaque = scanner.nextDouble();
+
+        if (valorSaque <= saldo) {
+            saldo = saldo - valorSaque;
+            System.out.println("Saque realizado com sucesso. Seu saldo é " + saldo);
+        }else{
+            System.out.println("Saldo insuficiente");
+            System.out.println("Este é seu saldo atual: " + saldo);
+            System.out.println("Volte e realize uma nova tentativa de saque");
+        }
+
+        scanner.close();
+    
+    }
+}
