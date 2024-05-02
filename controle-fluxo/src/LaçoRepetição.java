81f103b008edbf733fed293fb07a0c1336a94b74
Elisson Neves <elisson.gnl@gmail.com>
2024-05-02 16:38:37 -0300

    controle de fluxo

---
+++ controle-fluxo/src/LaçoRepetição.java
@@ -0,0 +1,30 @@
+public class LaçoRepetição {
+    
+    public static void main(String[] args) {
+        
+
+        for ( int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println(carneirinhos + " Carneirinhos");
+        }
+    
+        System.out.println(" Joãozinho dormiu");
+    
+        String alunos []= {"Elisson", "Mateus", "Eulemaque", "Murilo"};
+        for ( String aluno : alunos){
+            System.out.println("\nNome do aluno é: " + aluno);
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
